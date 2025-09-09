baebf072647968c5c084c72708989069e7c9872e
antrxc <uiantric06@gmail.com>
2025-09-09 08:32:08 +0000

    Added seperate summary generation

---
+++ OAIconfig/google_client.py
@@ -0,0 +1,8 @@
+from google import genai
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+GEMINI_API_KEY = os.getenv("GEMINI_API_KEY")
+
+client = genai.Client(api_key=GEMINI_API_KEY)
---
+++ OAIconfig/openai_client.py
@@ -0,0 +1,11 @@
+from openai import OpenAI
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+GEMINI_API_KEY = os.getenv("GEMINI_API_KEY")
+
+client = OpenAI(
+    api_key=GEMINI_API_KEY,
+    base_url="https://generativelanguage.googleapis.com/v1beta/openai/"
+)

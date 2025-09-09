0890bc0b90fb36711a2025bf4c01e38b267fc9ed
antrxc <uiantric06@gmail.com>
2025-09-09 10:46:44 +0000

    Starting RAG development

---
+++ RAG/ingest.py
@@ -0,0 +1,6 @@
+'''
+This is the file that uploads the processed text to the vectorDB
+TODO add embedding model, schema of the vectordb, etc.. 
+TODO add logic behind chunking and indexing of the file 
+TODO check if there are other ways to use the embeddings instead if loading the whole file into the model context
+'''
